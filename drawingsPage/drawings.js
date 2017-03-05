3dd92d8c2e20feaea3db8508c2dd005317a87695
Aaron-Buckles <ajb5551@gmail.com>
2017-03-04 20:29:19 -0800

    Added filter.  Added image viewer

---
+++ drawingsPage/drawings.js
@@ -0,0 +1,76 @@
+var tranSpeed = 0.3;
+var blurAmount = 10;
+
+function openViewer(img){
+	document.getElementById("viewedImage").src = img; // Set the image
+	document.getElementById("picViewer").style.height = "100%"; // Open the overlay
+
+	// Add the blur effect 
+	var body = document.getElementById("pageBody");
+	body.style.webkitFilter = "blur(" + blurAmount + "px)";
+	body.style.transition = "all " + tranSpeed + "s";
+}
+
+function closeViewer(){
+	document.getElementById("picViewer").style.height = "0%"; // Close the viewer
+
+	//Remove the blur effect 
+	document.getElementById("pageBody").style.webkitFilter = "none";
+}
+
+var type = "all"; // The default type of drawing
+function getType(){
+	var types = document.getElementsByName("type");
+	for(var i = 0; i < types.length; i++){
+		// If a radio is checked
+		if(types[i].checked){
+			// Don't apply same filter 2 times in a row
+			if(types[i].value != type){
+				type = types[i].value;
+				filter(type); // Call a function filtering the drawings
+			}
+
+			//Since only one radio can be checked at a time, break
+			break;
+		}
+	}
+}
+
+function filter(type){
+	// Get all of the images with these class names
+	var all = document.getElementsByClassName("all");
+	var oil = document.getElementsByClassName("oil");
+	var pencil = document.getElementsByClassName("pencil");
+	var pen = document.getElementsByClassName("pen");
+
+	if(type == "all"){
+		// Show all
+		$(".all").css("display", "initial");
+	}else if(type == "oil"){
+		// Show oil
+		$(".oil").css("display", "initial");
+
+		// Hide non-oil
+		$(".pencil").css("display", "none");
+		$(".pen").css("display", "none");
+
+	}else if(type == "pencil"){
+		// Show pencil
+		$(".pencil").css("display", "initial");
+
+		// Hide non-pencil
+		$(".oil").css("display", "none");
+		$(".pen").css("display", "none");
+
+	}else if(type == "pen"){
+		// Show pen
+		$(".pen").css("display", "initial");
+
+		// Hide non-pen
+		$(".oil").css("display", "none");
+		$(".pencil").css("display", "none");
+
+	}else{
+		console.log("This type does not exist!");
+	}
+}
