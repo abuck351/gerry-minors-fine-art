9291a79801f0e43bcd5b63480e2b7ef77711aa83
Aaron Buckles <abuck351@gmail.com>
2018-09-11 16:54:11 -0700

    Css grid and search box

--- resources/js/gallery.js
+++ resources/js/gallery.js
@@ -1,18 +1,7 @@
-var tranSpeed = 0.3;
-var blurAmount = 10;
-
 function openViewer(img, title){
 	document.getElementById("viewedImage").src = img; // Set the image
 	document.getElementById("picViewer").style.height = "100%"; // Open the overlay
-	console.log(title);
-	document.getElementById('viewerImageTitle').innerHTML = title;
-
-	// Add the blur effect 
-	/*
-	var body = document.getElementById("pageBody");
-	body.style.webkitFilter = "blur(" + blurAmount + "px)";
-	body.style.transition = "all " + tranSpeed + "s";
-	*/
+	document.getElementById('viewerImageTitle').innerHTML = title;	
 
 	// Prevent scrolling underneath the overlay
 	document.getElementsByTagName("BODY")[0].style.overflow = "hidden";
@@ -21,74 +10,30 @@ function openViewer(img, title){
 function closeViewer(){
 	document.getElementById("picViewer").style.height = "0%"; // Close the viewer
 
-	// Remove the blur effect 
-	// document.getElementById("pageBody").style.webkitFilter = "none";
-
 	// Reenable scrolling
 	document.getElementsByTagName("BODY")[0].style.overflow = "initial";
 	document.getElementById("viewedImage").src = "";
 }
 
-var type = "all"; // The default type of drawing
-function getType(){
-	var types = document.getElementsByName("type");
-	for(var i = 0; i < types.length; i++){
-		// If a radio is checked
-		if(types[i].checked){
-			// Don't apply same filter 2 times in a row
-			if(types[i].value != type){
-				type = types[i].value;
-				filter(type); // Call a function filtering the drawings
-			}
+// TODO: Allow searching of medium type
+function searchImages(){
+	var searchBox = document.getElementById("searchBox");
+	var searchText = searchBox.value.toUpperCase();
+	var panels = document.getElementsByClassName("panel");
 
-			//Since only one radio can be checked at a time, break
-			break;
+	for (var i = 0; i < panels.length; i++){
+		var firstP = panels[i].getElementsByTagName("p")[0];
+		if (firstP == null){
+			continue;
 		}
-	}
-}
 
-function filter(type){
-	if(type == "all"){
-		// Show all
-		$(".all").css("display", "initial");
-		
-	}else if(type == "oil"){
-		// Hide non-oil
-		$(".pencil").css("display", "none");
-		$(".penInk").css("display", "none");
-		$(".other").css("display", "none");
+		var title = firstP.innerHTML.toUpperCase();
 
-		// Show oil
-		$(".oil").css("display", "initial");
-
-	}else if(type == "pencil"){
-		// Hide non-pencil
-		$(".oil").css("display", "none");
-		$(".penInk").css("display", "none");
-		$(".other").css("display", "none");
-
-		// Show pencil
-		$(".pencil").css("display", "initial");
-
-	}else if(type == "penInk"){
-		// Hide non-penInk
-		$(".oil").css("display", "none");
-		$(".pencil").css("display", "none");
-		$(".other").css("display", "none");
-
-		// Show mixed
-		$(".penInk").css("display", "initial");
-
-	}else if(type == "other"){
-		// Hide non-other
-		$(".oil").css("display", "none");
-		$(".pencil").css("display", "none");
-		$(".penInk").css("display", "none");
-
-		// Show other
-		$(".other").css("display", "initial");
-
-	}else{
-		console.log("This type does not exist!");
+		if (title.indexOf(searchText) > -1 ){
+			panels[i].style.display = "initial";
+		}
+		else{
+			panels[i].style.display = "none";
+		}
 	}
 }
